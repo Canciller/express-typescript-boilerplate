3d8144e49da8a806cff3a50e1b0469e9ffae9ff1
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2022-01-14 16:08:24 -0600

    :tada: Initial commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' } )
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import hello from './hello';
+import helloWorld from '~/utils/helloWorld';
+
+console.log(hello());
+helloWorld();
